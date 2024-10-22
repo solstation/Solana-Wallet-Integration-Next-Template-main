e651eb3b397a2d3ad6135af8ded7bcf1026c1dbf
SolStation <solstationgit@outlook.com>
2024-10-23 07:22:59 +0900

    init commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+export default function Header() {
+    return (
+        <header>
+            <div className="header-content">
+                <WalletModalProvider>
+                    <WalletMultiButton />
+                </WalletModalProvider>
+            </div>
+        </header>
+    )
+}
